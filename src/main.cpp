b45069e431daec121b82bd66f81c5607b9adceeb
unknown <Felixlindblad.fl@gmail.com>
2020-09-03 11:23:52 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+int analogPin = A0;
+int val = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  val = analogRead(analogPin);
+  delay(1000);
+  //Serial.println(val);
+  float volatage = 5.0 * val / 1023;
+  Serial.println(volatage);
+}
